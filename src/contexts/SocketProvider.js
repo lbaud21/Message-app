f03445e577d4698aa3068ffff411b99ba5a9943c
Lucas BAUDINO <lucas.baudino@outlook.fr>
2021-02-17 18:08:28 +0100

    socket provider io url set to default

--- src/contexts/SocketProvider.js
+++ src/contexts/SocketProvider.js
@@ -11,7 +11,7 @@ export function SocketProvider({ username, children }) {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const newSocket = io("http://localhost:5000", { query: { username } });
+    const newSocket = io({ query: { username } });
     setSocket(newSocket);
     return () => newSocket.close();
   }, [username]);
